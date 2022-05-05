c1e9e140283b19ac4fbdaeba68ee532766348284
willow-rubenstein <contact.notashe@gmail.com>
2022-05-05 11:19:56 -0400

    cloud save

--- src/pages/error.js
+++ src/pages/error.js
@@ -4,7 +4,7 @@ export default function Error() {
     return (
         <>
             Page not found.<br/>
-            <Link to="/">Go to home page</Link>
+            <Link id="link" to="/">Go to home page</Link>
         </>
     )
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,20 @@
 import { Component } from 'react';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import "../styles/components/nav.css";
 
 export default class Home extends Component {
     render() {
         return (
             <div>
-                Homepage or something idk
+                <div className="nameHeader">
+                    Willow Rubenstein
+                </div>
+                Wow willow is so fucking cool holy shit wow omg no way I wanna meet willow they seem so cool
+                omg wow this is just a shit ton of placeholder text omg no way
+                <SyntaxHighlighter language="javascript" style={dark}>
+                    console.log("something idk lol");
+                </SyntaxHighlighter>
             </div>
         )
     }
